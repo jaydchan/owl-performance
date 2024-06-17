8d39b0917e4065b7352b5ba782f5a997676915e5
Jennifer Warrender <jaydchan@gmail.com>
2024-06-17 12:03:03 +0100

    Order data items and round average

--- plot.py
+++ plot.py
@@ -120,7 +120,8 @@ if __name__ == "__main__":
         plot_box_plots(data)
     else:
         # calc average
-        data = { key : mean(times) for (key, times) in data.items() }
+        data = { key : round(mean(times), 1) for (key, times) in sorted(data.items()) }
+        print(data)
 
         # parse data
         lines = {}
